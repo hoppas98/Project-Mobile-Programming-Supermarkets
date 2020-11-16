fa98ae40fc8abbe35e79846881d048f44e82a154
hoppas98 <58078678+hoppas98@users.noreply.github.com>
2020-11-16 05:21:05 +0200

    Add files via upload
    New updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,13 @@
-import 'package:flutter_app/pages/checkout.dart';
-import 'package:flutter_app/pages/checkout_final.dart';
-import 'package:flutter_app/pages/shop_items.dart';
-import 'package:flutter_app/pages/checkout_Confirmation_Message.dart';
-import 'package:flutter_app/pages/Details.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app_project/itemsView.dart';
 
 void main() => runApp(myApp());
 
 class myApp extends StatelessWidget {
-  @override
   Widget build(BuildContext context) {
-    Map<int, Color> color = {
-      50: Color.fromRGBO(255, 144, 0, .1),
-      100: Color.fromRGBO(255, 144, 0, .2),
-      200: Color.fromRGBO(255, 144, 0, .3),
-      300: Color.fromRGBO(255, 144, 0, .4),
-      400: Color.fromRGBO(255, 144, 0, .5),
-      500: Color.fromRGBO(255, 144, 0, .6),
-      600: Color.fromRGBO(255, 144, 0, .7),
-      700: Color.fromRGBO(255, 144, 0, .8),
-      800: Color.fromRGBO(255, 144, 0, .9),
-      900: Color.fromRGBO(255, 144, 0, 1),
-    };
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: MaterialColor(0xFFFF7000, color),
-      ),
-      initialRoute: '/shop_items',
-      routes: {
-        '/shop_items': (BuildContext context) => ShopItems(),
-        '/Details': (BuildContext context) => Details(),
-        '/checkout': (BuildContext context) => Checkout(),
-        '/checkout_final': (BuildContext context) => Checkout_final(),
-        '/checkout_Confirmation_Message': (BuildContext context) =>
-            Checkout_Confirmation_Mesage()
-      },
+      theme: ThemeData.light(),
+      home: itemsView(),
     );
   }
 }
