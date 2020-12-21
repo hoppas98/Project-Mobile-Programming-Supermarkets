be206155f681a178990a8996a335690bac2a900d
abdelrahmann98 <57917856+abdelrahmann98@users.noreply.github.com>
2020-12-21 02:19:58 +0200

    Cart added

--- Otlobly/test/widget_test.dart
+++ Otlobly/test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:abdo/main.dart';
+import 'package:Otlobly/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
