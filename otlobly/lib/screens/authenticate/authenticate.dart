f20d6113aef6af33405399319d82999864bad789
Mohamed Adel <hoppas9898@gmail.com>
2020-12-30 19:45:54 +0200

    Lastest version
    Admin Side
    30/12/2020

---
+++ otlobly/lib/screens/authenticate/authenticate.dart
@@ -0,0 +1,17 @@
+import 'package:otlobly/screens/authenticate/register.dart';
+import 'package:flutter/material.dart';
+import 'package:otlobly/screens/authenticate/sign_in.dart';
+
+class Authenticate extends StatefulWidget {
+  @override
+  _AuthenticateState createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: SignIn(),
+    );
+  }
+}
---
+++ otlobly/lib/screens/home/homeadmin.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
+import 'package:otlobly/screens/admin/manage_product.dart';
+import 'package:otlobly/screens/admin/addProducts.dart';
+import 'package:otlobly/services/auth.dart';
+
+class HomeAdmin extends StatefulWidget {
+  @override
+  _HomeAdminState createState() => _HomeAdminState();
+}
+
+class _HomeAdminState extends State<HomeAdmin> {
+  final AuthService _auth = AuthService();
+
+  @override
+  Widget imageCarousel = Container(
+    padding: EdgeInsets.all(10),
+    height: 300.0,
+    child: Carousel(
+      boxFit: BoxFit.cover,
+      images: [
+        AssetImage('images/Slider_p1.jpg'),
+        AssetImage('images/Slider_p2.jpg'),
+        AssetImage('images/Slider_p3.jpg'),
+        AssetImage('images/Slider_p4.jpg'),
+        AssetImage('images/Slider_p5.jpg'),
+      ],
+      autoplay: true,
+      dotSize: 4.0,
+      indicatorBgPadding: 2.0,
+      dotBgColor: Colors.transparent,
+      animationCurve: Curves.fastOutSlowIn,
+      animationDuration: Duration(milliseconds: 1000),
+    ),
+  );
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Admin'),
+        backgroundColor: Colors.red,
+        actions: [
+          FlatButton.icon(
+            onPressed: () async {
+              await _auth.signOut();
+            },
+            icon: Icon(
+              Icons.person,
+              color: Colors.white,
+            ),
+            label: Text('Logout',
+                style: TextStyle(
+                  color: Colors.white,
+                )),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              imageCarousel,
+              Spacer(),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: RaisedButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => AddProduct()));
+                  },
+                  color: Colors.red,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                      side: BorderSide(color: Colors.red)),
+                  child: Text('Add Products'),
+                ),
+              ),
+              Spacer(),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: FlatButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Editproduct()));
+                  },
+                  color: Colors.red,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                      side: BorderSide(color: Colors.red)),
+                  child: Text('Edit/Delete Products'),
+                ),
+              ),
+              Spacer(),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: RaisedButton(
+                  onPressed: () {},
+                  color: Colors.red,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                      side: BorderSide(color: Colors.red)),
+                  child: Text('View Orders'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
